1124d5ddedf851cb819e31ce9e6a65971d58ddbc
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-09-23 17:06:51 -0700

    Update init.js

--- src/init.js
+++ src/init.js
@@ -2,13 +2,15 @@ import { Error404 } from "./pages/404.js";
 import { Home } from "./pages/home.js";
  
 import  VaderRouter from "router"
-let app = new VaderRouter('/');
-app.use('/')
-app.get("/", async (req, res)=>{
+let app = new VaderRouter('/home');
+app.use('/home')
+ 
+app.get("/home", async (req, res)=>{
      res.render('#root', await Home.render())
 })
-app.on('/', async (req, res)=>{
+app.on('/home', async (req, res)=>{
         res.render('#root', await Home.render())
 })
+ 
  
 app.start()
