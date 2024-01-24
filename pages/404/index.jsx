2fcbe4bfe6b86c1aa3416fe771a6bafd58cd4b95
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-01-24 11:35:40 -0930

    Create index.jsx

---
+++ pages/404/index.jsx
@@ -0,0 +1,8 @@
+export default function (req, res) {
+    return <>
+        <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
+            <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
+            <a href="#/" className="text-blue-500    hover:underline">Go Back</a>
+        </div>
+    </>
+}
