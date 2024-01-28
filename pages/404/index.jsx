316bd30a30725b8b4244ca10c6280f0375a713af
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-01-28 12:33:11 -0930

    fix logo url

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -3,7 +3,7 @@ import { Layout } from '../../src/layout.jsx'
 export default function (req, res) {
     return (
         <>
-         <Layout {...{title: 'Malik Whitten', description: 'Malik Whitten Personal Website', logo: logo}}>
+         <Layout {...{title: 'Malik Whitten', description: 'Malik Whitten Personal Website', logo: '/public/logo.png'}}>
         <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
             <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
              <Link key="home" href="/" className="text-blue-500  cursor-pointer  hover:underline"
