6e2e73967205f55e939b8a0224f7d88c99f508a0
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-01-28 12:28:32 -0930

    fixed formatting

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -2,7 +2,8 @@ import {Link} from 'vaderjs/client'
 import { Layout } from '../../src/layout.jsx'
 export default function (req, res) {
     return (
-        <Layout {...{title: 'Malik Whitten', description: 'Malik Whitten Personal Website', logo: logo}}>
+        <>
+         <Layout {...{title: 'Malik Whitten', description: 'Malik Whitten Personal Website', logo: logo}}>
         <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
             <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
              <Link key="home" href="/" className="text-blue-500  cursor-pointer  hover:underline"
@@ -10,5 +11,6 @@ export default function (req, res) {
              >Go Home</Link>
         </div>
         </Layout>
+        </>
     )
 }
