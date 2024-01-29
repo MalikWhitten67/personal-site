3262867a560da835837162cf040477c4854b63f7
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-01-29 11:46:56 -0930

    remove logo - causing the process to hang

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -2,7 +2,7 @@ import {Link} from 'vaderjs/client'
 import { Layout } from '../../src/layout.jsx'
 export default function (req, res) {
     return (
-        <Layout $={{title: '404 | Hit a Brick Wall', description: 'If your seeing this you got lost :{', logo: logo}}>
+        <Layout $={{title: '404 | Hit a Brick Wall', description: 'If your seeing this you got lost :{'}}>
         <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
             <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
              <Link key="home" href="/" className="text-blue-500  cursor-pointer  hover:underline"
