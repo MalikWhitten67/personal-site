8a6cde5a3a94ddfa7b790b8aff4a65c3db5e4d33
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-01-29 12:04:46 -0930

    test for hanging

--- pages/404/index.jsx
+++ pages/404/index.jsx
@@ -3,14 +3,14 @@ import { Layout } from '../../src/layout.jsx'
 export default function (req, res) {
     return (
         <>
-            <Layout $={{ title: 'Malik Whitten', description: 'Malik Whitten Personal Website'}} >
-                <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
-                    <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
-                    <Link key="home" href="/" className="text-blue-500  cursor-pointer  hover:underline"
+           <Layout $={{title: '404 | Hit a Brick Wall', description: 'If your seeing this you got lost :{'}}>
+        <div className=" hero h-screen mx-auto justify-center    flex flex-col gap-5   ">
+            <h1 className="text-2xl font-bold">404 | Page Not Found</h1>
+             <Link key="home" href="/" className="text-blue-500  cursor-pointer  hover:underline"
 
-                    >Go Home</Link>
-                </div>
-            </Layout>
+             >Go Home</Link>
+        </div>
+        </Layout>
         </>
     )
 }
