381ac7d7042c3e38c3e10fb00863dbb0bb460a89
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2024-04-15 08:31:35 -0600

    Update footer.jsx

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <div className="footer">
             Copyright © 2023 / 2024 - Malik Whitten
 
-            Built with VaderJS v1.3.4
+            Built with VaderJS v1.4.6
 
             Source code available on <a href="https://github.com/MalikWhitten67/personal-site" target="_blank">Github</a>
         </div>
